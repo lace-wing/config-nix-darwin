33c988b4fc68ad6298e99288f73d26d1f81f8a6e
lace-wing <lycrlsu01@gmail.com>
2025-12-08 01:10:55 -0800

    flake: use flake-parts

--- flake.nix
+++ flake.nix
@@ -16,77 +16,27 @@
       flake = false;
     };
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs @ {
-    self,
-    nix-darwin,
+    flake-parts,
     nixpkgs,
-    nix-homebrew,
-    home-manager,
-    # flake-parts,
+    self,
     ...
-  }: let
-    configuration = {
-      pkgs,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} (top @ {
       config,
+      withSystem,
+      moduleWithSystem,
       ...
     }: {
-      environment.systemPackages = with pkgs; [
-        mkalias
-        vim
+      systems = ["aarch64-darwin"];
+      imports = [
+        ./darwin.nix
       ];
-
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = ["/Applications"];
-        };
-      in
-        pkgs.lib.mkForce ''
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix\ Apps/$app_name"
-          done
-        '';
-
-      nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 6;
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in {
-    darwinConfigurations.default = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        /*
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = "steve";
-            mutableTaps = false;
-            taps = with inputs; {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-            };
-          };
-        }
-        */
-      ];
-    };
-
-    darwinPackages = self.darwinConfigurations.default.pkgs;
-  };
+    });
 }
