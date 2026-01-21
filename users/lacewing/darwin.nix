2abae1ddf577dc3dba9ca678ce2059f94b6d2875
lace-wing <lycrlsu01@gmail.com>
2026-01-20 16:55:33 -0800

    more darwin config

--- users/lacewing/darwin.nix
+++ users/lacewing/darwin.nix
@@ -9,11 +9,36 @@
     };
 
     NSGlobalDomain = {
-      # Auto-hide menu bar
-      _HIHideMenuBar = true;
+      ### Control ###
       # Natural scrolling
       "com.apple.swipescrolldirection" = false;
+      # Spring loading on folders
+      "com.apple.springing.enabled" = true;
+
+      ### Appearance ###
+      # Auto-hide menu bar
+      _HIHideMenuBar = true;
+
+      ### Trackpad ###
+      # Tap to click
+      "com.apple.mouse.tapBehavior" = true;
+
+      ### Sound ###
+      # Beep when volume changed
+      "com.apple.sound.beep.feedback" = 0;
+      # Beep volume (1/e * e^x or 0 when x = 0)
+      "com.apple.sound.beep.volume" = 0.6065307;
     };
+
+    dock = {
+      enable-spring-load-actions-on-all-items = true;
+    };
+
+    WindowManager = {
+      EnableStandardClickToShowDesktop = false; # Only in State Manager
+    };
+
+    SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
   };
 
   system.keyboard = {
