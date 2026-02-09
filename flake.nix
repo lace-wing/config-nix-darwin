dda6ceb9ccdfee6e7952ef220579d39819fc2cfa
lace-wing <lycrlsu01@gmail.com>
2026-02-09 08:32:06 -0800

    flake update; pin zls on stable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    nixpkgs-stable = {
+      url = "github:NixOs/nixpkgs/4dadbbb8976a6f291c250f6546b55c2651238c2a";
+    };
+
     darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -37,6 +41,7 @@
   outputs = inputs @ {
     self,
     nixpkgs,
+    nixpkgs-stable,
     home-manager,
     darwin,
     ...
@@ -47,6 +52,7 @@
 
     overlays = [
       (final: prev: {
+        zls = nixpkgs-stable.legacyPackages.${prev.stdenv.hostPlatform.system}.zls;
       })
     ];
   in {
